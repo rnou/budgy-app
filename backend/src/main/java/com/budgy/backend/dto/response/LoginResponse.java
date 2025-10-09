d9bc99b6ee083e12ee4bcc558479906566165152
Renato Leite <renato.leite@eidosmedia.com>
2025-10-09 22:03:51 +0100

    add Jwt dep and configs

---
+++ backend/src/main/java/com/budgy/backend/dto/response/LoginResponse.java
@@ -0,0 +1,19 @@
+package com.budgy.backend.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String token;
+    private String type;
+    private String email;
+    private String name;
+    private Long userId;
+}
