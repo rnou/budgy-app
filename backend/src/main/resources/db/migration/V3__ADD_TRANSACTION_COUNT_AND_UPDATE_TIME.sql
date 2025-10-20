ca3630197cd9f0a86540bf36937f02e84c95d65e
rnou <dangelrenato@gmail.com>
2025-10-20 15:28:52 +0100

    fix: Dashboard Service

--- backend/src/main/resources/db/migration/V3__ADD_TRANSACTION_COUNT_AND_UPDATE_TIME.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE budgets
-    ADD transaction_count INT NULL;
-
-ALTER TABLE budgets
-    MODIFY transaction_count INT NOT NULL;
-
-ALTER TABLE transactions
-    ADD updated_at datetime NULL;
--- backend/src/main/resources/db/migration/V4__ADD_SAVING_TRANSACTION_COUNT.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE saving_pots
-    ADD transaction_count INT NULL;
-
-ALTER TABLE saving_pots
-    MODIFY transaction_count INT NOT NULL;
